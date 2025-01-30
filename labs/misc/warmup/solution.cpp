5c16b09a271cd07f06495cee6b17c1b0cd83f71c
vlabra <vlad.abramovich.28@gmail.com>
2025-01-30 19:35:07 +0000

    Solve warmup

--- labs/misc/warmup/solution.cpp
+++ labs/misc/warmup/solution.cpp
@@ -4,6 +4,10 @@ int solution(int *arr, int N) {
   // int res = 0;
   // for (int i = 0; i < N; ++i) {
   //   res += arr[i] + 1;
+<<<<<<< HEAD
   // }
+=======
+  // } qwe asd
+>>>>>>> ff14d0b (Solve warmup)
   return N * (N + 1) / 2;
 }
